8844b975de5e32a7a299e5b26a5e4060165ea7a8
szarobsky <szarob@me.com>
2024-03-07 14:28:53 -0500

    turned time output into clock format

--- server.js
+++ server.js
@@ -19,13 +19,13 @@ FRONTEND SENDS TO BACKEND IN FOLLOWING FORMAT:
     src: "2811 Einstein Way, Orlando, FL", //source address
     dest: "4000 Central Florida Blvd, Orlando, FL", //destination address
     stops: ["Miami, FL", "Tampa, FL", "Gainesville, FL"], //array of stop addresses
-    times: [3600, 7200, 10800], //array of times at each stop in seconds
-    startTime: 8400 //start time in seconds into the day
+    times: ["3600", "7200", "10800"], //array of times at each stop in seconds (can be given as string or number)
+    startTime: "8400" //start time in seconds into the day (can be given as string or number)
 
 DATA SENT TO FRONTEND IN FOLLOWING FORMAT:
 	stopOrder: bestRoute.routes[0].waypoint_order, //array of indicies of stops in order
 	distances: distances, //array of distances between stops (index 0 for src to stop at index 0, index 1 for stop at index 0 to index 1, etc.)
-	arrivalTimes: arrivalTimes //array of arrival times in seconds into the day (index 0 for arrival at stop at index 0, index 1 for arrival at stop at index 1, etc.)
+	arrivalTimes: arrivalTimes //array of arrival times in clock format (index 0 for arrival at stop at index 0, index 1 for arrival at stop at index 1, etc.)
 
 ERRORS MESSAGES TO FRONTEND:
 	"error - startTime is not a number" - startTime is not a number
@@ -44,6 +44,30 @@ function metersToMiles(meters) {
 	return meters * 0.000621371;
 }
 
+//converts seconds into clock format
+function secondsToClock(seconds) {
+	console.log("Starting secondsToClock with " + seconds);//REMOVE
+	let AmPm = "AM";
+	let hours = Math.floor(seconds / 3600);
+	seconds = seconds % 3600;
+	let minutes = Math.round(seconds / 60);
+
+	//adjusts if it is PM
+	if ((hours % 24) > 12) {
+		hours -= 12;
+		AmPm = "PM";
+	}
+	//adjusts if it is midnight
+	else if ((hours % 24) == 0)
+		hours = 12;
+
+	//adds 0 if minutes is less than 10
+	if (minutes < 10)
+		return hours + ":0" + minutes + " " + AmPm;
+	else
+		return hours + ":" + minutes + " " + AmPm;
+}
+
 //responds to post request
 app.post('/trip', async (req, res) => {  
 
@@ -55,6 +79,12 @@ app.post('/trip', async (req, res) => {
 	let stops = req.body.stops; //array of stop addresses
 	let times = req.body.times; //array of times at each stop in seconds
 	let startTime = parseFloat(req.body.startTime); //start time seconds into the day
+
+	console.log(src);
+	console.log(dest);
+	console.log(stops);
+	console.log(times);
+	console.log(startTime);
 
 	//validates startTime
 	if (isNaN(startTime)) {
@@ -162,7 +192,8 @@ async function receiveRequest(src, dest, stops, times, startTime) {
 	//get best route
 	let bestRoute = await getBestRoute(src, dest, stops);
 
-	//initializing arrays for travel times and distances
+	//initializing arrays for travel times in seconds and in string formart, and distances
+	let arrivalSec = [];
 	let arrivalTimes = [];
 	let distances = [];
 
@@ -177,14 +208,15 @@ async function receiveRequest(src, dest, stops, times, startTime) {
 
 		//based off of previous arrival time if not the first stop
 		if (i > 0)
-			arrivalTimes.push(bestRoute.routes[0].legs[i].duration.value + times[i - 1] + arrivalTimes[i - 1]);
+			arrivalSec.push(bestRoute.routes[0].legs[i].duration.value + times[i - 1] + arrivalSec[i - 1]);
 		else
-			arrivalTimes.push(bestRoute.routes[0].legs[i].duration.value);
+			arrivalSec.push(bestRoute.routes[0].legs[i].duration.value);
 	}
 
-	//arrival times are adjusted based on start time
-	for (let i = 0; i < arrivalTimes.length; i++) {
-		arrivalTimes[i] = (arrivalTimes[i] + startTime) % 86400;
+	//arrival times are adjusted based on start time and converted to clock format
+	for (let i = 0; i < arrivalSec.length; i++) {
+		arrivalSec[i] = (arrivalSec[i] + startTime) % 86400;
+		arrivalTimes[i] = secondsToClock(arrivalSec[i]);
 	}
 
 	console.log(distances);
